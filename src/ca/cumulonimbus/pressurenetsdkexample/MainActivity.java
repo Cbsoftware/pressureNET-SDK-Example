a2fffc58960905057fbe88c44299dac02d86bdfb
Jacob Sheehy <jacob.sheehy@gmail.com>
2013-10-06 12:04:16 -0400

    Adding buttons, connecting to CbService

--- src/ca/cumulonimbus/pressurenetsdkexample/MainActivity.java
+++ src/ca/cumulonimbus/pressurenetsdkexample/MainActivity.java
@@ -1,17 +1,48 @@
 package ca.cumulonimbus.pressurenetsdkexample;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import ca.cumulonimbus.pressurenetsdk.CbService;
 
 public class MainActivity extends Activity {
+	
+	private Button buttonStartService;
+	private Button buttonCheckSettings;
+	private Button buttonChangeSetting;
+	private Button buttonGetRecentReadings;
+	private Button buttonStopAutoSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        setClickListeners();
     }
 
+    private void setClickListeners() {
+    	buttonStartService = (Button) findViewById(R.id.buttonStartService);
+    	buttonCheckSettings = (Button) findViewById(R.id.buttonCheckSettings);
+    	buttonChangeSetting = (Button) findViewById(R.id.buttonChangeSetting);
+    	buttonGetRecentReadings = (Button) findViewById(R.id.buttonGetRecentReadings);
+    	buttonStopAutoSubmit = (Button) findViewById(R.id.buttonStopAutoSubmit);
+    	
+    	buttonStartService.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {	
+				System.out.println("starting service");
+				Intent intent = new Intent(getApplicationContext(), CbService.class);
+				startService(intent);
+			}
+		});
+    	
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
